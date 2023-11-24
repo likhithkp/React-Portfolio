d7cbed6e8d069a45b06b3d13c3fb965020ad46ca
Likhith K.P <likhithkpnet@gmail.com>
2023-11-24 07:13:36 +0530

    Taiwlind updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,13 +9,13 @@ const Contact = () => {
       <form
         method="POST"
         action="https://getform.io/f/78b27cb5-c604-4ec8-8abe-86542680800f"
-        className="flex flex-col max-w-[600px] w-full"
+        className="flex flex-col max-w-[600px] w-full h-screen mt-28 mb-8"
       >
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300">
             Contact
           </p>
-          <p className="text-gray-300 py-4">
+        <p className="text-gray-300 py-4">
             // Submit the form below or shoot me an email -
             likhithkpnet@gmail.com
           </p>
