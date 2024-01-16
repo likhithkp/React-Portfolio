d68f9549607c7467d44c647e8662b8b2f3c1d5e2
Nubunduram <ceantoine@hotmail.fr>
2024-01-16 12:12:41 +0100

    hover updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,66 +1,36 @@
 import React from "react";
-import { data } from "../model/data.js";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import { Link } from "react-scroll";
 
-const Work = () => {
-  // projects file
-  const project = data;
-  //setProject(data);
-
+const Home = () => {
   return (
-    <div name="work" className="w-full h-screen md:h-screen text-gray-300 bg-[#0a192f]">
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full">
-        <div className="pb-8 mt-24">
-          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
-            Work
-          </p>
-          <p className="py-6">// Check out some of my recent work</p>
-        </div>
-
-        {/* container for projects */}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {/* Grid Item */}
-          {project.map((item, index) => (
-            <div
-              key={index}
-              style={{ backgroundImage: `url(${item.image})` }}
-              className="shadow-lg shadow-[#040c16] group container rounded-md 
-              flex justify-center text-center items-center mx-auto content-div "
-            >
-              {/* Hover effect for images */}
-              <div className="hover-effect w-full h-full rounded-md grid place-items-center opacity-0 group-hover:opacity-100 ">
-                <span className="text-2xl font bold text-white tracking-wider ">
-                  {item.name}
-                </span>
-
-                <div className="pt-8 text-center ">
-                  {/* eslint-disable-next-line */}
-                  <a href={item.github} target="_blank">
-                    <button
-                      className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
-                    >
-                      Code
-                    </button>
-                  </a>
-                  {/* eslint-disable-next-line */}
-                  <a href={item.live} target="_blank">
-                    <button
-                      className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
-                    >
-                      Live
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          ))}
+    <div name="home" className="bg-[#0a192f] w-full h-screen">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          Likhith K.P.
+        </h1>
+        <h3 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I am a Software Developer.
+        </h3>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          As a developer, I intricately weave brilliance into the fabric of every codebase. With a relentless dedication from conception to execution, I sculpt seamless digital experiences that push the boundaries of what's possible.
+        </p>
+        <div>
+          <Link to="work" smooth={true} duration={500}>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+              View Work
+              <span>
+                <HiArrowNarrowRight className="ml-4 group-hover:rotate-90 duration-300" />
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </div>
   );
 };
 
-export default Work;
-
+export default Home;
 
