78b5f09640f96c04c5597320456fca1c70b4a06d
likhithkp <likhithkpdev@gmail.com>
2024-05-17 07:49:10 +0530

    skill updat

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -51,7 +51,7 @@ const Skills = () => {
             <p className="my-4">REACT</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-4">
-            <img className="w-20 mx-auto" src={Nextjs} alt="HTML icon" />
+            <img className="w-20 h-30 mx-auto" src={Nextjs} alt="HTML icon" />
             <p className="my-4">Next.js</p>
           </div>
           <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500 p-4">
